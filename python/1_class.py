675d516fe89767f920ebc434cb1fceed1752a706
Marko Jevtic <marjevtic@gmail.com>
2023-06-08 13:43:50 +0200

    Python - class example

---
+++ python/1_class.py
@@ -0,0 +1,22 @@
+class Cat:
+    def __init__(self, name, gender, age, weight, color):
+        self.name = name
+        self.gender = gender
+        self.age = age
+        self.weight = weight
+        self.color = color
+    
+    def breath(self):
+        pass
+    
+    def eat(food):
+        pass
+    
+    def run(destination):
+        pass
+    
+    def sleep(hours):
+        pass
+    
+    def meow():
+        pass
