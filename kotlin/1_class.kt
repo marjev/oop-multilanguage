ef8a5cce20312f763d1ba5c2827536922a3785d0
Marko Jevtic <marjevtic@gmail.com>
2023-06-08 15:06:44 +0200

    Kotlin - class example

---
+++ kotlin/1_class.kt
@@ -0,0 +1,13 @@
+class Cat {
+    var name: String? = null
+    var gender: String? = null
+    var age: Int? = null
+    var weight: Float? = null
+    var color: String? = null
+
+    fun breath() {}
+    fun eat(food: String) {}
+    fun run(destination: String) {}
+    fun sleep(hours: Int) {}
+    fun meow() {}
+}
