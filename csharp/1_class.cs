de1b8221e074571309f760fdc3b8667c573e1f4b
Marko Jevtic <marjevtic@gmail.com>
2023-06-08 14:38:55 +0200

    Class - C# example

---
+++ csharp/1_class.cs
@@ -0,0 +1,14 @@
+public class Cat
+{
+    public string name;
+    public string gender;
+    public int age;
+    public float weight;
+    public string color;
+    
+    public void breath() {}
+    public void eat(string food) {}
+    public void run(string destination) {}
+    public void sleep(int hours) {}
+    public void meow() {}
+}
